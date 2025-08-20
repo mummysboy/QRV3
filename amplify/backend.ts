d6dda4b53eb3d6874a4345012a78824892190dbe
mummysboy <89770985+mummysboy@users.noreply.github.com>
2025-08-20 16:59:22 -0700

    Trigger deployment - Add deployment timestamp and comments to backend config

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -1,6 +1,10 @@
-import { defineBackend } from "@aws-amplify/backend";
-import { data } from "./data/resource";
-import { storage } from "./storage/resource";
+// Amplify Gen 2 Backend Configuration
+// Deployment trigger: 2025-08-20 23:50 UTC - API fixes deployed
+// All endpoints now working with direct DynamoDB calls
+
+import { defineBackend } from '@aws-amplify/backend';
+import { data } from './data/resource';
+import { storage } from './storage/resource';
 
 export const backend = defineBackend({
   data,
